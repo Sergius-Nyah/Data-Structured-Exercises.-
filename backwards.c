607b699dced2102e5adcf26e4787ebfd5d051cfc
サージ <74214119+Sergius-Nyah@users.noreply.github.com>
2023-01-31 16:43:29 +0100

    Update backwards.c
    Generalised for 'n' positive Intergers.

--- backwards.c
+++ backwards.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 #include<stdlib.h>
+#include<math.h>
 int main (void)
 {
-int number = 711;
-int lastDigit = number%100;
-int firstDigit = number/100;
+int the_number;
+printf("Enter the number you wish to reverse:\n");
+scanf("%d", &the_number);
+  if(the_number>0)
+  {
+int length = floor(log10(abs(the_number))) + 1;  //Gets the length of the interger. 
+int lastDigit = the_number%(10^(length-1));
+int firstDigit = the_number/(10^(length-1));
 printf("%i%i\n",lastDigit,firstDigit);
+  }
+  else
+    printf("Invalid number, Enter a positive number"); 
+return 0;
 }
+
